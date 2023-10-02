e863d8b86e93c94157c34dbd7b911988d5b3e22d
Devanshukoli <kolidevanshu02@gmail.com>
2023-10-02 09:53:42 +0530

    <typo>: client/src//lib/browserNotification.js , in this file I have just fixed some typos with prettier format.

--- client/src/lib/browserNotification.js
+++ client/src/lib/browserNotification.js
@@ -1,18 +1,18 @@
 
 export const requestBrowserNotificationPermissions = () => {
-  if (!("Notification" in window)) {
-    console.log("Browser does not support desktop notification");
-  } else {
-    Notification.requestPermission();
-  }
+    if (!("Notification" in window)) {
+        console.log("Browser does not support desktop notification");
+    } else {
+        Notification.requestPermission();
+    }
 }
 
 export const createBrowserNotification = (title, body) => {
-  if (Notification.permission === 'denied') {
-    return
-  }
+    if (Notification.permission === 'denied') {
+        return
+    }
 
-  new Notification(title, {
-    body, icon: '/favicon.ico',
-  })
+    new Notification(title, {
+        body, icon: '/favicon.ico',
+    })
 }
